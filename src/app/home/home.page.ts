095e52a5be5b3ca25fc63f0150fb446ac083f096
Phillip Hartin <mail@philliphartin.com>
2018-10-10 09:36:24 +1100

    Adds basic example of press and pressup events

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  public tapCount: number = 0;
+    public tapCount: number = 0;
+    public pressCount: number = 0;
+    public pressState: string = "released";
 
-  tapButton(){
-      this.tapCount = this.tapCount + 1;
-  }
+    tapButton() {
+        this.tapCount = this.tapCount + 1;
+    }
+
+    onPress($event) {
+        console.log("On Press", $event);
+        this.pressState = 'pressing';
+    }
+
+     onPressUp($event) {
+        console.log("On Pressup", $event);
+         this.pressState = 'released';
+     }
 
 }
